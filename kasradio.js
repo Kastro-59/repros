19117b7bb1f72fd40fcd7babe90bbe71f5e69b9e
Kastro-59 <84104324+Kastro-59@users.noreply.github.com>
2023-05-29 23:52:21 +0200

    Add files via upload

--- kasradio.js
+++ kasradio.js
@@ -1925,13 +1925,13 @@ a.ajax({
 type: "GET",
 crossDomain: !0,
 cache: !1,
-url: "https://api2.xatblog.net/currentsong?stream=https://stream.zeno.fm/" + Bc ,
+url: "https://api.streamafrica.net/zeno/index.php?z=" + Bc ,
 dataType: "json",
 async: !0,
 success: function(a) {
 if (a) try {
-var b = a.message + " - " + a.title;
-v != b && (v = b, ya(), "small" == y ? P() : (M = a.message, N = a.title, l(), H()))
+var b = a.currentSong +  " - " + a.title;
+v != b && (v = b, ya(), "small" == y ? P() : (M = a.currentSong +  " - " + a.currentArtist, N = a.title, l(), H()))
 } catch (A) {}
 },
 error: function(a, b, c) {}
